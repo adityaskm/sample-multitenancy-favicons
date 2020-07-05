5f6e357999d315fe9d9b420466fedb68ac194da0
Aditya Mudgerikar <aditya18thm@gmail.com>
2020-07-05 15:29:08 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { TENANT_LIST } from './shared/constants/tenants.const';
+import { Tenant } from './shared/models/tenant.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'multitenancy';
+
+  tenantList = TENANT_LIST;
+
+  private _selectedTenant: Tenant = this.tenantList[0];
+  public get selectedTenant(): Tenant {
+    return this._selectedTenant;
+  }
+  public set selectedTenant(value: Tenant) {
+    this._selectedTenant = value;
+    this.setSiteFavicon();
+  }
+
+  setSiteFavicon() {
+    const faviconLink = document.getElementById(
+      'site-favicon'
+    ) as HTMLLinkElement;
+    faviconLink.href = this.selectedTenant.iconUrl;
+  }
 }
---
+++ src/app/shared/constants/tenants.const.ts
@@ -0,0 +1,29 @@
+import { Tenant } from '../models/tenant.model';
+
+export const TENANT_LIST: Tenant[] = [
+  {
+    id: 1,
+    name: 'Tenant 1',
+    iconUrl: 'assets/favicons/tenant1/favicon.ico',
+  },
+  {
+    id: 2,
+    name: 'Tenant 2',
+    iconUrl: 'assets/favicons/tenant2/favicon.ico',
+  },
+  {
+    id: 3,
+    name: 'Tenant 3',
+    iconUrl: 'assets/favicons/tenant3/favicon.ico',
+  },
+  {
+    id: 4,
+    name: 'Tenant 4',
+    iconUrl: 'assets/favicons/tenant4/favicon.ico',
+  },
+  {
+    id: 5,
+    name: 'Tenant 5',
+    iconUrl: 'assets/favicons/tenant5/favicon.ico',
+  },
+];
